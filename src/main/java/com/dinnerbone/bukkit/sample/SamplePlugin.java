fd50056e71ce8fd601f78acefb9e294fae60e731
stevenh <steven.hartland@multiplay.co.uk>
2011-01-04 15:51:09 +0000

    Cleaned up example code, adding comments and optimising

--- src/main/java/com/dinnerbone/bukkit/sample/SamplePlugin.java
+++ src/main/java/com/dinnerbone/bukkit/sample/SamplePlugin.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
 /**
  * Sample plugin for Bukkit
@@ -23,25 +24,35 @@ public class SamplePlugin extends JavaPlugin {
 
     public SamplePlugin(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File plugin, ClassLoader cLoader) {
         super(pluginLoader, instance, desc, plugin, cLoader);
+        // TODO: Place any custom initialisation code here
 
-        registerEvents();
+        // NOTE: Event registration should be done in onEnable not here as all events are unregistered when a plugin is disabled
     }
 
     public void onDisable() {
+        // TODO: Place any custom disable code here
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
         System.out.println("Goodbye world!");
     }
 
     public void onEnable() {
-        System.out.println("Hello world!");
-    }
+        // TODO: Place any custom enable code here including the registration of any events
 
-    private void registerEvents() {
-        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Normal, this);
-        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, playerListener, Priority.Normal, this);
-        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_COMMAND, playerListener, Priority.Normal, this);
-        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_MOVE, playerListener, Priority.Normal, this);
-        getServer().getPluginManager().registerEvent(Event.Type.BLOCK_PHYSICS, blockListener, Priority.Normal, this);
-        getServer().getPluginManager().registerEvent(Event.Type.BLOCK_CANBUILD, blockListener, Priority.Normal, this);
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_QUIT, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_COMMAND, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_PHYSICS, blockListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_CANBUILD, blockListener, Priority.Normal, this);
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
     }
 
     public boolean isDebugging(final Player player) {
